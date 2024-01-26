d125cfa7416fcf12b1f868905200ca01a8ebfe0a
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-01-26 23:43:32 +1300

    Finished the logic

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import tkinter as tk
+from random import randint
 
-def rollDice():
-    print("rolling dice")
-    return 1
+def rollDice(appIn, messageIn):
+    value = randint(0,6)
+    messageIn.config(text="The dice roll is: "+str(value))
+    messageIn.update()
 
 app = tk.Tk()
-message = tk.Message(app, text="Welcome to the dice roller", width=150)
-message.config(bg='orange')
-message.pack()
+welcomeMessage = tk.Message(app, text="Welcome to the dice roller", width=150)
+welcomeMessage.config(bg='orange')
+welcomeMessage.pack()
 
-rollButton = tk.Button(app, text='Roll', width=25, command=rollDice())
+output = tk.Message(app, text="", width=300)
+output.pack()
+
+rollButton = tk.Button(app, text='Roll', width=25, command=lambda: rollDice(app, output))
 rollButton.pack()
 
 app.mainloop()
